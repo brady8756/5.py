981885f6ca803fc04103d79aee974ee3ed4a65bf
brady8756 <95857599+brady8756@users.noreply.github.com>
2022-01-06 22:30:51 +0800

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,44 +1,150 @@
+from typing import List, Tuple
+from string import ascii_lowercase
+import types
 import random
+import dataclasses
+
 import streamlit as st
 
-class Die:
-    def __init__(self, sides):
-        self.sides = sides
-        self.keep = False
-        self.save_value()
+from gamestate import persistent_game_state
 
-    def save_value(self):
-        self.value = self.roll()
+PROTOTYPE = """
+ ┏━━┑
+ ┃  O>
+ ┃>╦╧╦<
+ ┃ ╠═╣
+ ┃ ╨ ╨
+ ┻━━━━
+"""
 
-    def roll(self):
-        return random.randint(1, self.sides)
 
-st.title("Tenzi")
+STEPS = [
+"""
+ ┏━━┑
+ ┃
+ ┃
+ ┃
+ ┃
+ ┻━━━━
+""",
+"""
+ ┏━━┑
+ ┃  O
+ ┃
+ ┃
+ ┃
+ ┻━━━━
+""",
+"""
+ ┏━━┑
+ ┃  O>
+ ┃
+ ┃
+ ┃
+ ┻━━━━
+""",
+"""
+ ┏━━┑
+ ┃  O>
+ ┃ ╔╧╗
+ ┃ ╚═╝
+ ┃
+ ┻━━━━
+""",
+"""
+ ┏━━┑
+ ┃  O>
+ ┃>╦╧╗
+ ┃ ╚═╝
+ ┃
+ ┻━━━━
+""",
+"""
+ ┏━━┑
+ ┃  O>
+ ┃>╦╧╦<
+ ┃ ╚═╝
+ ┃
+ ┻━━━━
+""",
+"""
+ ┏━━┑
+ ┃  O>
+ ┃>╦╧╦<
+ ┃ ╠═╝
+ ┃ ╨
+ ┻━━━━
+""",
+"""
+ ┏━━┑
+ ┃  O>
+ ┃>╦╧╦<
+ ┃ ╠═╣
+ ┃ ╨ ╨
+ ┻━━━━
+"""
+]
 
-if 'dice' not in st.session_state:
-    st.session_state.dice = []
+MIN_LENGTH = 3
+MAX_LENGTH = 8
 
-NUM_DICE = 10
-NUM_SIDES = 6
-if st.button("Roll"):
-    if len(st.session_state.dice) == 0:
-        for idx in range(NUM_DICE):
-            st.session_state.dice.append(Die(NUM_SIDES))
-    else:
-        for idx, die in enumerate(st.session_state.dice):
-            if not die.keep:
-                st.session_state.dice[idx].save_value()
+@st.cache
+def get_words() -> List[str]:
+    with open('words1000.txt') as f:
+        words = [line.strip() for line in f]
 
-current_value = 0
-has_won = False
-if len(st.session_state.dice) > 0:
-    current_value = st.session_state.dice[0].value
-    has_won = True
-for idx, die in enumerate(st.session_state.dice):
-    die.keep = st.checkbox(str(die.value), key=str(idx))
-    if die.value != current_value:
-        has_won = False
+    words = [w for w in words if MIN_LENGTH <= len(w) <= MAX_LENGTH]
+    words = [w for w in words if all('a' <= c <= 'z' for c in w)]
 
-if has_won:
-    st.write("You win!")
-    st.balloons()
+    return words
+
+@dataclasses.dataclass
+class GameState:
+    game_number: int
+    word: str
+    guessed: Tuple[str, ...] = ()
+    step: int = 0
+    game_over: bool = False
+
+state = persistent_game_state(initial_state=GameState(0, random.choice(get_words()))) 
+
+if st.button("new game"):
+    state.guessed = ()
+    state.step = 0
+    state.game_number += 1
+    state.word = random.choice(get_words())
+    state.game_over = False
+
+if not state.game_over:
+    guess = st.text_input("guess a letter", max_chars=1, key=state.game_number)
+
+    if not guess:
+        st.write("please guess")
+    elif guess < 'a' or guess > 'z':
+        st.write("please guess a lowercase letter!")
+    elif guess in state.guessed:
+        st.write(f"you already guessed **{guess}**")
+    elif guess not in state.word:
+        st.write(f"the word has no **{guess}**")
+        state.step += 1
+        state.guessed += (guess,)
+    else: 
+        st.write("good guess")
+        state.guessed += (guess,)
+
+if state.step == len(STEPS) - 1:
+    st.markdown(f"you lose, the word was **{state.word}**")
+    state.game_over = True
+elif all(c in state.guessed for c in state.word):
+    st.markdown(f"**YOU WIN**")
+    state.game_over = True
+
+# Show the chicken
+st.text(STEPS[state.step])
+
+# Show the word
+chars = [c if c in state.guessed else "_" for c in state.word]
+st.text(" ".join(chars))
+
+# Show the guessed letters
+st.text(f'guessed: {" ".join(state.guessed)}')
